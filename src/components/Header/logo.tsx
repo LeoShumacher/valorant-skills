9fd8e75823cd50ab1d350ccbc0a8512721b4d273
LeoShumacher <leo21devrj@gmail.com>
2024-01-12 19:52:05 -0300

    remove: back arrow in desktop header

--- src/components/Header/logo.tsx
+++ src/components/Header/logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link className="text-3xl font-bold" href={"/"}>
+        <Link className="text-3xl sm:font-4xl font-bold" href={"/"}>
             VALORANT
         </Link>
     )
