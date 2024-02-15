d9713df1feb15ab2d9729333243246e62a97ae01
LeoShumacher <leo21devrj@gmail.com>
2024-02-14 21:36:00 -0300

    add: Agents components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Banner from "@/components/MainContent/banner";
 
+
+
 export default function Home() {
   return (
     <div className="flex flex-col ">
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,12 +9,18 @@ export default function Header() {
   const pathname = usePathname();
   return (
     <div className="h-12 w-full flex items-center justify-between px-1 sm:px-4 z-10">
-      {pathname !== "/" ? <Link className="sm:hidden" href={"/"}><ArrowLeftIcon className="w-7 h-7 text-white" /></Link> : ""}
-        <Logo />
-        <Menu />
-        <div className="hidden sm:flex">
+      {pathname !== "/" ? (
+        <Link className="sm:hidden" href={"/"}>
+          <ArrowLeftIcon className="w-7 h-7 text-white" />
+        </Link>
+      ) : (
+        ""
+      )}
+      <Logo />
+      <Menu />
+      <div className="hidden sm:flex">
         <MenuContent />
-        </div>
+      </div>
     </div>
   );
 }
